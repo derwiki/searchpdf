cfaee559de5ab3b86535a705e4f922ae656c1abf
Adam Derewecki <derewecki@gmail.com>
2017-03-29 09:00:19 -0700

    WIP

--- Gemfile
+++ Gemfile
@@ -7,8 +7,20 @@ gem 'pg', group: :production
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
-gem 'google_cloud_vision'
+gem 'google-cloud-vision'
 gem 'rmagick'
+gem 'pdfkit'
+gem 'pdf-reader'
+
+install_if -> { RUBY_PLATFORM =~ /darwin/ } do
+  # Includes wkthmltopdf binaries for use on Mac OS X
+  gem 'wkhtmltopdf-binary-edge', '~> 0.12.3.0'
+end
+
+install_if -> { RUBY_PLATFORM =~ /linux/ } do
+  # Includes wkthmltopdf binaries for use on Heroku/AWS
+  gem 'wkhtmltopdf-heroku'
+end
 
 group :development, :test do
   gem 'sqlite3'
--- lib/ocr_pdf.rb
+++ lib/ocr_pdf.rb
@@ -1,18 +1,24 @@
 require 'google/cloud/vision'
+require 'log_helper'
 
 class OcrPdf
+  include LogHelper
   attr_accessor :pdf_filename
 
   def initialize(pdf_filename)
     fail "GOOGLE_CLOUD_KEYFILE env variable required" unless ENV['GOOGLE_CLOUD_KEYFILE']
     fail "Input must be PDF" unless pdf_filename&.split('.')&.last&.downcase == 'pdf'
     self.pdf_filename = pdf_filename
+    log :pdf_filename, pdf_filename
   end
 
   def perform
     [].tap do |pages|
-      Magick::ImageList.new(self.pdf_filename).each_with_index do |page_img, i|
+      image_list = Magick::ImageList.new(self.pdf_filename)
+      log :image_list_count, image_list.count
+      image_list.each_with_index do |page_img, i|
         tempfile = Tempfile.new(%w(image .jpg))
+        log :path, tempfile.path
         begin
           log :writing_image, tempfile.path
           page_img.write(tempfile.path)
