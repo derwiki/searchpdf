4815dc38f19025cc26f061d43ec6c102505f87a9
Adam Derewecki <adam@snapdocs.com>
2017-03-31 16:21:01 -0700

    Rip out image/pdf libraries in favor of shelling to ghostscript

--- Gemfile
+++ Gemfile
@@ -8,19 +8,6 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'google-cloud-vision'
-gem 'rmagick'
-gem 'pdfkit'
-gem 'pdf-reader'
-
-install_if -> { RUBY_PLATFORM =~ /darwin/ } do
-  # Includes wkthmltopdf binaries for use on Mac OS X
-  gem 'wkhtmltopdf-binary-edge', '~> 0.12.3.0'
-end
-
-install_if -> { RUBY_PLATFORM =~ /linux/ } do
-  # Includes wkthmltopdf binaries for use on Heroku/AWS
-  gem 'wkhtmltopdf-heroku'
-end
 
 group :development, :test do
   gem 'sqlite3'
--- lib/ocr_pdf.rb
+++ lib/ocr_pdf.rb
@@ -1,43 +1,70 @@
 require 'google/cloud/vision'
 require 'log_helper'
 
-class OcrPdf
-  include LogHelper
-  attr_accessor :pdf_filename
+module GoogleCloudVision
+  class OcrPdfService
+    attr_accessor :pdf_filename
 
-  def initialize(pdf_filename)
-    fail "GOOGLE_CLOUD_KEYFILE env variable required" unless ENV['GOOGLE_CLOUD_KEYFILE']
-    fail "Input must be PDF" unless pdf_filename&.split('.')&.last&.downcase == 'pdf'
-    self.pdf_filename = pdf_filename
-    log :pdf_filename, pdf_filename
-  end
+    def initialize(pdf_filename)
+      fail "GOOGLE_CLOUD_KEYFILE env variable required" unless ENV['GOOGLE_CLOUD_KEYFILE']
+      fail "Input must be PDF" unless pdf_filename&.split('.')&.last&.downcase == 'pdf'
+      self.pdf_filename = pdf_filename
+      log :page_count, self.page_count
+    end
 
-  def perform
-    [].tap do |pages|
-      image_list = Magick::ImageList.new(self.pdf_filename)
-      log :image_list_count, image_list.count
-      image_list.each_with_index do |page_img, i|
-        tempfile = Tempfile.new(%w(image .jpg))
-        log :path, tempfile.path
-        begin
-          log :writing_image, tempfile.path
-          page_img.write(tempfile.path)
-          image = vision.image(tempfile.path)
-          annotation = vision.annotate(image, document: true)
+    def perform
+      [].tap do |pages|
+        self.page_count.times do |i|
+          tempfile = Tempfile.new(%w(image .jpg))
+          begin
+            log :writing_image, tempfile.path
+            write_pdf_page_to_jpg(i, tempfile.path)
+            log :compressing_image
+            log :loading_image
+            image = vision.image(tempfile.path)
+            image.context.languages = ['en-US']
+            log :annotating_image
+            annotation = vision.annotate(image, document: true)
+          ensure
+            tempfile.close
+            tempfile.unlink
+          end
+
+          log :extracting_text
           text = annotation.text.to_s
           log self.pdf_filename, i, text
+          yield [text, i] if block_given?
           pages << text
-        ensure
-          tempfile.close
-          tempfile.unlink
         end
       end
     end
+
+    protected
+
+      def page_count
+        @page_count ||= `gs -q -dNODISPLAY -c "(#{self.pdf_filename}) (r) file runpdfbegin pdfpagecount = quit";`.to_i
+      end
+
+    private
+
+      def vision
+        @vision ||= Google::Cloud::Vision.new(project: ENV['GOOGLE_CLOUD_PROJECT_ID'])
+      end
+
+      def write_pdf_page_to_jpg(page_num, file_path)
+        command = <<-EOS.squish
+          gs -sDEVICE=jpeg
+             -dBATCH
+             -dNOPAUSE
+             -r225
+             -sOutputFile=#{file_path}
+             -dFirstPage=#{page_num+1}
+             -dLastPage=#{page_num+1}
+             -dJPEGQ=40
+             #{self.pdf_filename}
+        EOS
+        `#{ command }`
+        log :file, file_path, :page_num, page_num, :bytes, File.size(file_path)
+      end
   end
-
-  private
-
-    def vision
-      @vision ||= Google::Cloud::Vision.new(project: ENV['GOOGLE_CLOUD_PROJECT_ID'])
-    end
 end
